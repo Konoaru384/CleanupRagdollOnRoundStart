cc5cde54a39037f7b7373bab4dc58fbe0bf911c2
Konoaru <jordan.jeanson31470@gmail.com>
2025-06-19 07:28:50 +0200

    Add files via upload

---
+++ CleanupRagdollOnStart.cs
@@ -0,0 +1,33 @@
+﻿using Exiled.API.Features;
+namespace Cleanup_Ragdoll_On_Start
+{
+    public class CleanupRagdollOnStart : Plugin<Config>
+    {
+        public override string Name { get; } = "CleanupRagdollOnStartBetterThisTime";
+
+        public override string Prefix { get; } = "CROSBT";
+
+        public override string Author { get; } = "Konoaru & Lanonymous";
+
+        // Create a plugin instance for easy config access, the null check isn't necessary but it's a good practice to avoid null reference exceptions.
+        public static CleanupRagdollOnStart Instance { get; private set; } = null;
+
+        // Subscribe for events here, you could do an event manager class if you want to keep things organized.
+        public override void OnEnabled()
+        {
+            // Instance this plugin so we can access it from anywhere.
+            Instance = this;
+            Exiled.Events.Handlers.Server.RoundStarted += EventHandler.RoundStartEvent.Instance.OnRoundStart;
+            base.OnEnabled();
+        }
+
+        // Unsubscribe from events here, you could do an event manager class if you want to keep things organized.
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted -= EventHandler.RoundStartEvent.Instance.OnRoundStart;
+            // Set the instance to null to allow for garbage collection and avoid memory leaks.
+            Instance = null;
+            base.OnDisabled();
+        }
+    }
+}
---
+++ EventHandler/RoundStartEvent.cs
@@ -0,0 +1,18 @@
+﻿using Exiled.API.Features;
+
+namespace Cleanup_Ragdoll_On_Start.EventHandler
+{
+    public class RoundStartEvent : Singleton<RoundStartEvent>
+    {
+        public void OnRoundStart()
+        {
+            Cleaning();
+        }
+
+        public static void Cleaning()
+        {
+            Map.CleanAllRagdolls();
+            Log.Debug("Ragdolls cleared.");
+        }
+    }
+}
