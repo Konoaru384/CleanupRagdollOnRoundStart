78c869a2c208d28ca3aeabb34c46275df6955f03
Konoaru <jordan.jeanson31470@gmail.com>
2025-06-19 07:39:23 +0200

    Update CleanupRagdollOnStart.cs

--- CleanupRagdollOnStart.cs
+++ CleanupRagdollOnStart.cs
@@ -1,4 +1,4 @@
-﻿using Exiled.API.Features;
+using Exiled.API.Features;
 namespace Cleanup_Ragdoll_On_Start
 {
     public class CleanupRagdollOnStart : Plugin<Config>
@@ -9,23 +9,18 @@ namespace Cleanup_Ragdoll_On_Start
 
         public override string Author { get; } = "Konoaru & Lanonymous";
 
-        // Create a plugin instance for easy config access, the null check isn't necessary but it's a good practice to avoid null reference exceptions.
         public static CleanupRagdollOnStart Instance { get; private set; } = null;
 
-        // Subscribe for events here, you could do an event manager class if you want to keep things organized.
         public override void OnEnabled()
         {
-            // Instance this plugin so we can access it from anywhere.
             Instance = this;
             Exiled.Events.Handlers.Server.RoundStarted += EventHandler.RoundStartEvent.Instance.OnRoundStart;
             base.OnEnabled();
         }
 
-        // Unsubscribe from events here, you could do an event manager class if you want to keep things organized.
         public override void OnDisabled()
         {
             Exiled.Events.Handlers.Server.RoundStarted -= EventHandler.RoundStartEvent.Instance.OnRoundStart;
-            // Set the instance to null to allow for garbage collection and avoid memory leaks.
             Instance = null;
             base.OnDisabled();
         }
